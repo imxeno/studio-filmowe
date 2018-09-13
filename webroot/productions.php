740ba665befb2fa1448a269aae19806febd17d50
xeno <xeno@xeno.yt>
2018-09-13 18:21:15 +0200

    productions now show first_name and last_name of contracting person

--- webroot/productions.php
+++ webroot/productions.php
@@ -7,7 +7,7 @@ if(!$_SESSION["id"]) {
 }
 
 $res = $DB->query("SELECT productions.id, productions.name, productions.premiere, productions.genre, productions.costs,"
-     . " contracting_id, users.first_name as contacting_first_name,"
+     . " contracting_id, users.first_name as contracting_first_name,"
      . " users.last_name as contracting_last_name FROM productions INNER JOIN users ON users.id = productions.contracting_id");
 
 $productions = $res->fetch_all(MYSQLI_ASSOC);
