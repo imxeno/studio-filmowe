22ca39254970c386698c30071cdea168efd2d197
xeno <xeno@xeno.yt>
2018-09-13 19:10:45 +0200

    some buttons here and there

--- webroot/staff.php
+++ webroot/staff.php
@@ -7,15 +7,18 @@ if(!$_SESSION["id"]) {
 }
 
 if(isset($_GET["id"])) {
+
+    $positions = $DB->query("SELECT id, name, access_level FROM positions WHERE access_level > 0 ORDER BY access_level DESC")->fetch_all(MYSQLI_ASSOC);
+
     $id = intval($_GET["id"]);
-    $res = $DB->query("SELECT users.id, users.first_name, users.last_name, users.address, users.phone FROM users"
+    $res = $DB->query("SELECT users.id, users.first_name, users.last_name, users.address, users.phone, users.position FROM users"
      . " INNER JOIN positions ON users.position = positions.id WHERE positions.access_level > 0 AND users.id = "
      . $id . " LIMIT 1")->fetch_assoc();
      if(!$res) {
         echo $twig->render('error.twig', array( 'error' => ("Pracownik o ID #" . $id . " nie istnieje.")));
         exit();
      }
-     echo $twig->render('worker.twig', array('worker' => $res));
+     echo $twig->render('worker.twig', array('worker' => $res, 'positions' => $positions));
      exit();
 }
 
