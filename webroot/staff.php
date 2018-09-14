faa9951cfc9c510add4e2acb009d4feb0db3eb9a
xeno <xeno@xeno.yt>
2018-09-14 20:29:25 +0200

    worker date and salary fixes

--- webroot/staff.php
+++ webroot/staff.php
@@ -9,13 +9,19 @@ if(isset($_GET["id"])) {
     $positions = $DB->query("SELECT id, name, access_level FROM positions WHERE access_level > 0 ORDER BY access_level DESC")->fetch_all(MYSQLI_ASSOC);
 
     $id = intval($_GET["id"]);
-    $res = $DB->query("SELECT users.id, users.first_name, users.last_name, users.address, users.phone, users.position FROM users"
+    $res = $DB->query("SELECT users.id, users.first_name, users.last_name, users.address, users.phone, users.position, users.salary," 
+     . " users.agreement_signed, users.agreement_expires FROM users"
      . " INNER JOIN positions ON users.position = positions.id WHERE positions.access_level > 0 AND users.id = "
      . $id . " LIMIT 1")->fetch_assoc();
      if(!$res) {
         echo $twig->render('error.twig', array( 'error' => ("Pracownik o ID #" . $id . " nie istnieje.")));
         exit();
      }
+     $res["salary"] = number_format($res["salary"], 2, '.', '');
+     $rgx = "/(\d+-\d+-\d+) (\d+:\d+):\d+/";
+     $rpl = "$1T$2";
+     $res["agreement_signed"] = preg_replace($rgx, $rpl, $res["agreement_signed"]);
+     $res["agreement_expires"] = preg_replace($rgx, $rpl, $res["agreement_expires"]);
      echo $twig->render('worker.twig', array('worker' => $res, 'positions' => $positions));
      exit();
 }
